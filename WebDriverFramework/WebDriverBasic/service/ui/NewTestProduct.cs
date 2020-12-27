de01151be039e225b2bd253ea8138f40c9329bd1
BaboshkinaDasha <belkadashuuta@gmail.com>
2020-12-28 00:10:30 +0300

    AddWebDriverFramework

---
+++ WebDriverFramework/WebDriverBasic/service/ui/NewTestProduct.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using TestWebDriverAdvanced.po;
+using WebDriverFramework.business_objects;
+
+namespace WebDriverFramework.service.ui
+{
+    class NewTestProduct
+    {
+        public static string NewProduct(Product testpr, IWebDriver driver)
+        {
+            Page_AllProducts page_allpr = new Page_AllProducts(driver);
+            Page_NewProduct newpr = new Page_NewProduct(driver);
+            Page_Homepage homepage = new Page_Homepage(driver);
+            homepage.ToAllProducts();
+            page_allpr.ToNewProduct();
+            newpr.TestNewProduct(testpr);
+            return page_allpr.CloseFormEdit();
+        }
+    }
+}
