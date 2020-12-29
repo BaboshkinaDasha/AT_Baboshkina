9a3f7f7c6338937d8aeceafa19202ceb7864b76c
BaboshkinaDasha <belkadashuuta@gmail.com>
2020-12-29 09:26:47 +0300

    AddWebDriverSpecflow_Edit

---
+++ WebDriverSpecflow_Edit/WebDriverBasic/po/Page_AllProducts.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using WebDriverFramework.business_objects;
+
+namespace TestWebDriverAdvanced.po
+{
+    class Page_AllProducts
+    {
+        private IWebDriver driver;
+
+        public Page_AllProducts(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement NewProduct => driver.FindElement(By.LinkText("Create new"));
+        private IWebElement EditProduct => driver.FindElement(By.LinkText("Fanta"));
+        private IWebElement CloseForm => driver.FindElement(By.XPath("//h2"));
+        private IWebElement RemoveProduct => driver.FindElement(By.XPath("//*[a[text()=\"Fanta\"]]/following-sibling::*[10]/a[text()=\"Remove\"]"));
+
+
+        public void ToNewProduct()
+        {
+            new Actions(driver).MoveToElement(NewProduct).Click(NewProduct).Build().Perform();
+        }
+
+        public void ToEditProduct(Product prod1)
+        {
+            driver.FindElement(By.LinkText(prod1.ProductName)).Click();
+        }
+
+        public string CloseFormEdit()
+        {
+            return CloseForm.Text;
+        }
+
+        public void ToRemoveProduct()
+        {
+            new Actions(driver).MoveToElement(RemoveProduct).Click(RemoveProduct).Build().Perform();
+            driver.SwitchTo().Alert().Accept();
+        }
+
+        public bool FindProduct(string productname)
+        {
+            return driver.FindElements(By.LinkText(productname)).Count != 0;
+        }
+    }
+}
---
+++ WebDriverSpecflow_Edit/WebDriverBasic/tests/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverFramework.tests
+{
+    [TestFixture]
+
+    public class BaseTest
+    {
+        //protected IWebDriver driver;
+
+        //[OneTimeSetUp]
+        //public void Setup()
+        //{
+        //driver = new ChromeDriver();
+        //driver.Navigate().GoToUrl("http://localhost:5000/");
+        //driver.Manage().Window.Maximize();
+        //}
+
+        //[OneTimeTearDown]
+        //public void CloseDriver()
+        //{
+        //driver.Close();
+        //driver.Quit();
+        //}
+        //}
+    }
+}
